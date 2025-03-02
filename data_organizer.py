cfbb7daec23fb3f78a43cc4f8ca05c549aea1417
Alex Dameron <AWDameron@gmail.com>
2025-03-02 11:22:51 -0500

    Removed duplicate posts, organized

--- data_organizer.py
+++ data_organizer.py
@@ -7,8 +7,8 @@ def emoji_organizer(text_list):
     emoji_list = []
     
     for entry in text_list:
-        emojis_found = [e['emoji'] for e in emoji.emoji_list(entry)]  # Extract emojis
-        emoji_list.extend(emojis_found)  # Collect emojis
+        emojis_found = [e['emoji'] for e in emoji.emoji_list(entry)] 
+        emoji_list.extend(emojis_found) 
 
     # Save emojis to CSV
     with open('post_emojis.csv', 'a', newline='', encoding='utf-8') as csvfile:
@@ -27,17 +27,23 @@ def data_organizer(list,csv_name):
             writer = csv.writer(csvfile,quotechar='"',quoting=csv.QUOTE_ALL)
             for entry in data:
                 text_list.append(entry['post_text'])
-                writer.writerow([entry['post_text'].replace("\n", "\\n")])
+            clean_list = set(text_list)
+            writer.writerow(clean_list)
 
-    emoji_organizer(text_list)
-    # print(file)
-    # print(len(text_list))
-    # print(text_list)
+    print(len(text_list))
+    print(len(clean_list))
+    print(f'{csv_name} has {len(text_list)-len(clean_list)} duplicates')
+    emoji_organizer(clean_list)
     return 
 
+# Turns data in to CSV files so they are easier to process
+# 170 duplicate posts of 1218 posts scraped.
+data_organizer(csv_lists.resume_list,'resume tips.csv')
+data_organizer(csv_lists.interview_list,'interview tips.csv')
+data_organizer(csv_lists.data_analysis_list,'data analysis tips.csv')
+data_organizer(csv_lists.job_list,'job tips.csv')
 
-#data_organizer(csv_lists.resume_list,'resume tips')
-data_organizer(csv_lists.interview_list,'interview tips')
-data_organizer(csv_lists.data_analysis_list,'data analysis tips')
-data_organizer(csv_lists.job_list,'job tips')
 print("data organized, emoji's organized")
+
+
+
